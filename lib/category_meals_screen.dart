cda328ec0370c440a76b1e4fac71a4e182ccf662
ayari17 <ayari17@hotmail.fr>
2021-03-02 22:30:38 +0100

    Added Meals Model and associated Dummy data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_memo/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
- static const routeName = '/category-meals';
- /* final String categoryId ;
+  static const routeName = '/category-meals';
+
+  /* final String categoryId ;
   final String categoryTitle;
 
   CategoryMealsScreen(this.categoryId,this.categoryTitle);*/
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String,String>;
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
+
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
     return Scaffold(
-      appBar: AppBar(title: Text(categoryTitle),),
-      body: Center(
-        child: Text(
-            'The Recipes For the Category'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length,
+        ));
   }
 }
